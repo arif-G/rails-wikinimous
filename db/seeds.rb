a5f596ea5099097ba63408040c6a7b15229372f3
Arif Gömleksiz <arifgoemleksiz101@gmail.com>
2021-05-18 14:34:53 +0200

    model and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  title = Faker::Hacker.say_something_smart
+  content = Faker::Lorem.paragraphs
+  article = Article.new({ title: title, content: content })
+  article.save
+end
